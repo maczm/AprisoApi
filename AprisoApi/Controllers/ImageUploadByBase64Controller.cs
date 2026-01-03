da8d088427356c808c007c82e81b7247a5d16aee
wangzm <1466418631@qq.com>
2026-01-03 17:30:18 +0800

    添加项目文件。

---
+++ AprisoApi/Controllers/ImageUploadByBase64Controller.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AprisoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageUploadByBase64Controller : ControllerBase
+    {
+    }
+}
